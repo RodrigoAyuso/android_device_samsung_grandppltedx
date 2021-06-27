d012b64dfd38ed73e666889fa3e70d154c4b4d5c
jmpfbmx <jmpf.bmx@gmail.com>
2021-06-27 12:28:57 +0200

    grandppltedx: Don't build binderize graphics allocator and graphics composer
     * We have them as passthrough at manifest.

--- device.mk
+++ device.mk
@@ -46,7 +46,6 @@ PRODUCT_PACKAGES += \
 	libui_ext \
 	libshim_gui \
 	android.hardware.graphics.allocator@2.0-impl \
-	android.hardware.graphics.allocator@2.0-service \
 	android.hardware.graphics.mapper@2.0-impl \
 	android.hardware.graphics.composer@2.1-impl
 
