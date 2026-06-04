e2b5c5419ae01541420de22c7bbe1379730ae803
RodrigoAyuso <rodrigo.mazzonetti@gmail.com>
2026-06-04 09:20:02 -0300

    device.mk: Import Pine Graphics settings
    This completely resolves the HWC problem, making the system run more smoothly.

--- device.mk
+++ device.mk
@@ -42,9 +42,10 @@ PRODUCT_PACKAGES += \
 
 # Graphics
 PRODUCT_PACKAGES += \
+	libGLES_android \
 	libgui_ext \
 	libui_ext \
-	libshim_gui \
+	libgralloc_extra \
 	android.hardware.graphics.allocator@2.0-impl \
 	android.hardware.graphics.mapper@2.0-impl \
 	android.hardware.graphics.composer@2.1-impl
