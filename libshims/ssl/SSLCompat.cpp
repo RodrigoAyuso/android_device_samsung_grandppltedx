1ef477a144294139a9f51fe1a3db809dd1a1ecf9
RodrigoAyuso <rodrigo.mazzonetti@gmail.com>
2026-08-15 22:17:04 -0300

    grandppltedx: shim legacy SSL methods for AGPS

--- libshims/ssl/SSLCompat.cpp
+++ libshims/ssl/SSLCompat.cpp
@@ -3,3 +3,7 @@
 extern "C" const SSL_METHOD* SSLv3_client_method(void) {
     return TLS_client_method();
 }
+
+extern "C" const SSL_METHOD* SSLv3_server_method(void) {
+    return TLS_server_method();
+}
